4d0ad9ff2fa725f69b25b11a9c64aacc98a2f169
WSCkarunamoorthy <karunamoorthy.it@westayclose.in>
2024-09-23 13:12:27 +0530

    first commit

---
+++ src/helloworldkarunamoorthy/demo.java
@@ -0,0 +1,16 @@
+package helloworldkarunamoorthy;
+
+import java.util.*;
+import java.io.*;
+
+public class demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int a = 10;
+int b = 5;
+ int c = a+b;
+	System.out.println(c);
+	}
+
+}
---
+++ src/helloworldkarunamoorthy/helloworld.java
@@ -0,0 +1,11 @@
+package helloworldkarunamoorthy;
+
+public class helloworld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("HELLO WORLD WELCOME KARUNA");
+
+	}
+
+}
